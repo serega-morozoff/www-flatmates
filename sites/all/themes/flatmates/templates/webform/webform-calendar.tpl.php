f38392b3da3784143826d348677af3f094a85751
Jack Chan <easysolutionaustralia@gmail.com>
2017-02-08 23:08:53 +1100

    fixing php tag bug

--- sites/all/themes/flatmates/templates/webform/webform-calendar.tpl.php
+++ sites/all/themes/flatmates/templates/webform/webform-calendar.tpl.php
@@ -9,4 +9,4 @@
 <?php if(arg(0) == "node" && arg(1) == "49"){?>
 	<?php //$idKey = str_replace('_', '-', $component['form_key']); ?>
 	<input type="text" id="edit-submitted-move-date" class="form-text <?php print implode(' ', $calendar_classes); ?>" alt="<?php print t('Open popup calendar'); ?>" title="<?php print t('Open popup calendar'); ?>" />
-<? } ?>
+<?php } ?>
